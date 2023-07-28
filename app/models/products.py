dad48832e1fd38857af7d5a6a0a7ac8777e1b1d4
Minh Tran <minhttran93@gmail.com>
2023-07-28 15:39:44 -0700

    add models

---
+++ app/models/products.py
@@ -0,0 +1,64 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+#Many-to-Many Relationship between Users & Products
+favorites = db.Table(
+  "favorites",
+  db.Column("userId", db.ForeignKey("user.id"), primary_key=True),
+  db.Column("productId", db.ForeignKey("product.id"), primary_key=True)
+)
+
+class Product(db.Model):
+    __tablename__ = "products"
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(255))
+    price = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    preview_imageURL = db.Column(db.String, nullable=False)
+    reviewId = db.Column(db.Integer, db.ForeignKey("review.id"))
+    sellerId = db.Column(db.Integer, db.ForeignKey("seller.id"), nullable=False)
+    createdAt = db.Column(db.DateTime)
+    updatedAt = db.Column(db.DateTime)
+
+    # One-to-Many Relationship with Product and ProductImage
+    # This relationship states that Product will be listening to the class ProductImage
+    image = db.relationship("ProductImage", back_populates="product")
+    users = db.relationship("User",secondary=favorites, back_populates="products")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'item_name': self.item_name,
+            'price': self.price,
+            'category': self.category,
+            'description': self.description,
+            "quantity": self.quantity,
+            "preview_imageURL": self.preview_imageURL,
+            "reviewId": self.reviewId,
+            "sellerId": self.sellerId,
+            "createdAt": self.createdAt,
+            "updatedAt": self.updatedAt
+        }
+
+
+class ProductImage(db.Model):
+  __tablename__ = "product_images"
+  id = db.Column(db.Integer, primary_key=True)
+  # other columns
+  productId = db.Column(db.Integer, db.ForeignKey("product.id"))
+  product_imageURL = db.Column(db.String(255), nullable=True)
+
+  # One-to-Many Relationship with Product and ProductImage
+  # This relationship states that ProductImage will be listening to the class Product
+  product = db.relationship("Product", back_populates="image")
+
+
+  def to_dict(self):
+    return {
+        "id": self.id,
+        "productId": self.productId,
+        "product_imageURL": self.product_imageURL
+    }
