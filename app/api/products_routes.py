77b02118a7ba4eb2ab4a2728fcdeedf91f023551
thandi mpofu <thandimpofu2003@gmail.com>
2023-07-29 16:55:35 -0500

    product_details route issue

--- app/api/products_routes.py
+++ app/api/products_routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint
-from flask_login import login_required
+from flask import Blueprint, request
+from flask_login import current_user
 from app.models import Product, User
-from ..app import login
+# from .. import login
+# from ..app import login
 
 products_routes = Blueprint('products', __name__)
 
@@ -15,6 +16,19 @@ def get_products():
 
         }
 
-@products_routes.route("/")
+# @login.user_loader
+@products_routes.route("/current")
 def user_products():
-    
+    # grabs current user instance and turns it into a dic that grabs the id
+    userId = current_user.to_dict()["id"]
+    user_products = Product.query.filter(Product.sellerId == userId).all()
+    return {
+
+        "Products": [p.to_dict() for p in user_products]
+
+    }
+
+@products_routes.route('/<int:id>')
+def product_details(id):
+    product = Product.query.get(id)
+    return "hi"
