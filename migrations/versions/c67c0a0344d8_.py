8dd256885ef5d191639e0631de90fc075696a7b4
Minh Tran <minhttran93@gmail.com>
2023-07-28 18:05:46 -0700

    some minor fixes from singular to plural for deployment

--- migrations/versions/c67c0a0344d8_.py
+++ migrations/versions/c67c0a0344d8_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 6ce13018865c
+Revision ID: c67c0a0344d8
 Revises: 
-Create Date: 2023-07-28 16:40:37.710190
+Create Date: 2023-07-28 18:04:22.038751
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6ce13018865c'
+revision = 'c67c0a0344d8'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -36,8 +36,6 @@ def upgrade():
     sa.Column('hashed_password', sa.String(length=255), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('email'),
-    sa.UniqueConstraint('first_name'),
-    sa.UniqueConstraint('last_name'),
     sa.UniqueConstraint('username')
     )
     op.create_table('products',
