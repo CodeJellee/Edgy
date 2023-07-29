8ed793e60a0ce5d27e4e70e201ef651112e73372
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-07-29 19:40:46 -0400

    add get reviews by current user, need to add products when assoc is fixed

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -1,57 +1,40 @@
 from flask import Blueprint
 from flask_login import login_required
-from ..models import Review, db
+from ..models import Review, User, db
 # from ..forms import ProductForm
+from flask_login import current_user, login_required
+from pprint import pprint
 
 reviews_routes = Blueprint('reviews', __name__)
 
 
-# @reviews_routes.routes("/current")
-# @login_required
-# def get_users_reviews():
-#     print("in ...reviews/current")
-#
-#     return "hi"
+# !!!!!!!!!!!!!!!!!!!! NEED TO ADD PRODUCT STILL
+@reviews_routes.route("/current", methods=['GET'])
+@login_required
+def post_product_review():
+    cur_user = current_user.to_dict()
+    pprint(cur_user)
+
+    reviews = Review.query.filter(Review.userId == cur_user["id"]).all()
+    reviews = [review.to_dict() for review in reviews]
+    # user = User.query.filter(User.email == form.data['email']).first()
+    returnItem = {"Reviews": reviews,
+                  "User": {
+                      "id": cur_user["id"],
+                      "firstName": cur_user["first_name"],
+                      "lastName": cur_user["last_name"],
+                  }
+                  }
+    pprint(returnItem)
+    # SimplePerson.query.filter(SimplePerson.name.startswith("M")).all()
+
+
+    # products = Product.to_dict().all()
+    return "hi"
 
 
 
-# @reviews_routes.routes("/<Int:productId>/reviews")
-# def get_product_reviews():
-#     print("in ...reviews/current")
-#
-#     return "hi"
-
-
-# @reviews_routes.routes("/<Int:productId>/reviews", methods=['GET'])
-# def post_product_review():
-#     # form = ProductForm()
-#     print("in ...reviews/current")
-#
-#     return "hi"
-
-
-# @reviews_routes.routes("/<Int:productId>/reviews", methods=['POST'])
-# def post_product_review():
-#     # form = ProductForm()
-#     print("in ...reviews/current")
-#
-#     return "hi"
-
-
-
-
-# @reviews_routes.route("/<Int:reviewId>", methods=['PUT'])
-# def post_product_review():
-#     # form = ProductForm()
-#     print("in ...reviews/current")
-#     # products = Product.to_dict().all()
-#     return "hi"
-
-
-
-
-# @reviews_routes.route("/<reviewId>", methods=[''])
-
+# @reviews_routes.route("/<int:reviewId>", methods=[''])
 # def post_product_review():
 #     # form = ProductForm()
 #     print("in ...reviews/current")
--- migrations/versions/272fa2ecf561_.py
+++ migrations/versions/272fa2ecf561_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: faecacf0d073
+Revision ID: 272fa2ecf561
 Revises: 
-Create Date: 2023-07-29 15:04:13.601344
+Create Date: 2023-07-29 18:16:53.114348
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'faecacf0d073'
+revision = '272fa2ecf561'
 down_revision = None
 branch_labels = None
 depends_on = None
