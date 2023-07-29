033350829aa3edf1f89bd3c05bad5b6661bfb0c9
Minh Tran <minhttran93@gmail.com>
2023-07-29 13:49:50 -0700

    remove error

--- app/api/products_routes.py
+++ app/api/products_routes.py
@@ -3,18 +3,16 @@ from flask_login import login_required
 from app.models import Product, User
 from ..app import login
 
-products_routes = Blueprint('products', __name__)
+products_routes = Blueprint("products", __name__)
+
 
 @products_routes.route("/")
 def get_products():
     products = Product.query.all()
     products = [p.to_dict() for p in products]
-    return {
+    return {"Products": products}
 
-        "Products": products
-
-        }
 
 @products_routes.route("/")
 def user_products():
-    
+    pass
