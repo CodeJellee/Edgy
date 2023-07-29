cd14897ed3b0b660c89da3bda6e0ce9a3aa1114b
thandi mpofu <thandimpofu2003@gmail.com>
2023-07-29 12:22:21 -0500

    import issues resolved

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,11 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 from .models import db, User
-# from .api.user_routes import user_routes
-from .api import reviews_routes, user_routes, auth_routes, cart_routes, products_routes
-# from .api.auth_routes import auth_routes
+from .api.user_routes import user_routes
+from .api.auth_routes import auth_routes
+from .api.cart_routes import cart_routes
+from .api.products_routes import products_routes
+from .api.reviews_routes import reviews_routes
 from .seeds import seed_commands
 from .config import Config
 
--- app/api/products_routes.py
+++ app/api/products_routes.py
@@ -3,3 +3,8 @@ from flask_login import login_required
 from app.models import Product
 
 products_routes = Blueprint('products', __name__)
+
+# @products_routes("")
+# def get_products():
+#     products = Product.to_dict().all()
+#     return products
