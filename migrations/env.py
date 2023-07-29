69094ab55929cb1d5d2fe0b762aa82c9f63973ac
Minh Tran <minhttran93@gmail.com>
2023-07-28 20:43:55 -0700

    add foreign key column to userId

--- migrations/env.py
+++ migrations/env.py
@@ -99,3 +99,9 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
+    context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
