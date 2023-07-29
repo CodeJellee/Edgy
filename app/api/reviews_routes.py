7d4f332d9b143c9afdab30fd8d8ea5127c220e5a
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-07-29 14:22:34 -0400

    reviews

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -1,5 +1,57 @@
 from flask import Blueprint
 from flask_login import login_required
-from app.models import Review
+from ..models import Review, db
+# from ..forms import ProductForm
 
 reviews_routes = Blueprint('reviews', __name__)
+
+
+# @reviews_routes.routes("/current")
+# @login_required
+# def get_users_reviews():
+#     print("in ...reviews/current")
+#  
+#     return "hi"
+
+
+
+# @reviews_routes.routes("/<Int:productId>/reviews")
+# def get_product_reviews():
+#     print("in ...reviews/current")
+#
+#     return "hi"
+
+
+# @reviews_routes.routes("/<Int:productId>/reviews", methods=['GET'])
+# def post_product_review():
+#     # form = ProductForm()
+#     print("in ...reviews/current")
+#
+#     return "hi"
+
+
+# @reviews_routes.routes("/<Int:productId>/reviews", methods=['POST'])
+# def post_product_review():
+#     # form = ProductForm()
+#     print("in ...reviews/current")
+#
+#     return "hi"
+
+
+
+
+@reviews_routes.routes("/<Int:reviewId>", methods=['PUT'])
+def post_product_review():
+    # form = ProductForm()
+    print("in ...reviews/current")
+    # products = Product.to_dict().all()
+    return "hi"
+
+
+
+@reviews_routes.routes("/<Int:reviewId>", methods=[''])
+def post_product_review():
+    # form = ProductForm()
+    print("in ...reviews/current")
+    # products = Product.to_dict().all()
+    return "hi"
