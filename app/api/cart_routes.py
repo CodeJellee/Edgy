e1d3ab94fb80e7878c26f948982aecc012c06407
Jennifer Lee <leejpac@gmail.com>
2023-07-30 17:18:48 -0400

    updated cart route get

--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -8,6 +8,38 @@ from pprint import pprint
 
 cart_routes = Blueprint('cart_items', __name__)
 
+# @cart_routes.route('/shopping_cart', methods=["GET"])
+# @login_required
+# def get_cart_items():
+#     cur_user = current_user.to_dict()
+#     shopping_cart = {"Shopping_Cart": []}
+
+#     #Retrieve cart products for the current user
+#     cart_items = CartItem.query.filter(CartItem.userId == cur_user["id"]).all()
+
+#     #Add the favorite products to the list
+#     for item in cart_items:
+#         product = Product.query.get(item.productId)
+#         if product:
+#             item_info = {
+#                 "id": item.id,
+#                 "userId": item.userId,
+#                 "Products": {
+#                     "id": product.id,
+#                     "sellerId": product.sellerId,
+#                     "itemName": product.itemName,
+#                     "price": product.price,
+#                     "description": product.description,
+#                     "previewImage": product.preview_imageURL,
+#                     "category": product.category
+#                 },
+#             }
+#             shopping_cart["Shopping_Cart"].append(item_info)
+#     return shopping_cart.to_dict()
+
+
+cart_routes = Blueprint('cart_items', __name__)
+
 @cart_routes.route('/shopping_cart', methods=["GET"])
 @login_required
 def get_cart_items():
@@ -16,16 +48,48 @@ def get_cart_items():
 
     #Retrieve cart products for the current user
     cart_items = CartItem.query.filter(CartItem.userId == cur_user["id"]).all()
-
     #Add the favorite products to the list
     for item in cart_items:
-        cart_products.append(item.to_dict())
+        # print("this is item", item.to_dict()) #{'id': 7, 'productId': 3, 'userId': 6}
+        i = item.to_dict()
+        product = Product.query.get(item.productId)
+        i["Product"] = product.to_dict()
+        cart_products.append(i)
+        # cart_products.append(product.to_dict())
 
-    print(cart_products)
-    # for favorite in favorites:
-    #     favorites = Product.query.all(cur_user["id"] == favorites.userId).all()
-    #     print(favorite)
-    return "<h1>Shopping Cart Page</h1>"
+
+    print(cart_products) #getting back an list of dict
+    return {"Shopping_Cart": cart_products}
+
+
+
+# @cart_routes.route('/shopping_cart', methods=["GET"])
+# @login_required
+# def get_cart_items():
+#     cur_user = current_user.to_dict()
+#     cart_products = {}
+#     cart_products["Shopping_Cart"] = []
+#     # cart_products["Product"] = {}
+#     #Retrieve cart products for the current user
+#     cart_items = CartItem.query.filter(CartItem.userId == cur_user["id"]).all()
+#     cart_products["Shopping_Cart"] = cart_items
+
+#     #Add the favorite products to the list
+#     for item in cart_items:
+#         item = item.to_dict()
+#         # cart_products.append(item.to_dict())
+#         product = Product.query.get(item.productId)
+#         item["Product"] = product.to_dict()
+#         del item["productId"]
+
+#         # cart_products["Product"]= product.to_dict()
+
+
+#     print(cart_products) #getting back an list of dict
+
+#     return cart_products
+
+
 
 
 @cart_routes.route('/shopping_cart/<int:productId>', methods=["DELETE"])
