9e82c413042a2029d10302259fdecf70e03d610a
thandi mpofu <thandimpofu2003@gmail.com>
2023-07-30 18:42:00 -0500

    componets for home and categories

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,6 +7,7 @@ import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 import ReviewsCurr from './components/Reviews/ReviewsCurr'
 import HomePage from "./components/HomePage";
+import Categories from "./components/Categories";
 
 function App() {
   const dispatch = useDispatch();
@@ -31,6 +32,30 @@ function App() {
           </Route>
           <Route path="/your_reviews">
             <ReviewsCurr />
+          </Route>
+          <Route exact path="/categories/clothing">
+            <Categories category="clothing"/>
+          </Route>
+          <Route exact path="/categories/home_decor">
+            <Categories category="home_decor"/>
+          </Route>
+          <Route exact path="/categories/accessories">
+            <Categories category="accessories"/>
+          </Route>
+          <Route exact path="/categories/computer">
+            <Categories category="computer"/>
+          </Route>
+          <Route exact path="/categories/waifu_body_pillows">
+            <Categories category="waifu_body_pillows"/>
+          </Route>
+          <Route exact path="/categories/books">
+            <Categories category="books"/>
+          </Route>
+          <Route exact path="/categories/music">
+            <Categories category="music"/>
+          </Route>
+          <Route exact path="/categories/figurines">
+            <Categories category="figurines"/>
           </Route>
           <Route path="/">
             <h2>404 not found</h2>
