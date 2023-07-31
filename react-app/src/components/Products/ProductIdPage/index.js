3a1401fdf848803886b227f4241907b93f8a4d4c
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-07-31 19:30:54 -0400

    product info and transaction buttons done

--- react-app/src/components/Products/ProductIdPage/index.js
+++ react-app/src/components/Products/ProductIdPage/index.js
@@ -68,67 +68,70 @@ function ProductIdPage() {
     return (
         <>
             {/* product info */}
-            <div className='PID-product'>
-                <div className='PID-all-Images'>
-                    <div className='PID-images-div'>
-                        <img className='PID-small-img' onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/9b902f/4473276055/il_794xN.4473276055_fcxk.jpg" alt="filler image 2"></img>
-                        <img className='PID-small-img' onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/2f8cab/4425918302/il_794xN.4425918302_1x8r.jpg" alt="filler image 3"></img>
-                        <img className='PID-small-img' onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/401a30/4473276305/il_794xN.4473276305_qrxw.jpg" alt="filler image 4"></img>
-                    </div>
-                    <div className='PID-main-image-div'>
-                        <img className='PID-main-image' onClick={changeMainImage} src={mainImage} alt="filler image"></img>
-                    </div>
-                </div>
-
-                <div>
-                    <div>
-                        <i onClick={addToFav} className="fas fa-heart" />
-                    </div>
-
-
-                    <div className='PID-about-buttons-div'>
-
-
-                        <div className='PID-about-product-div'>
-                            <p>{product.price}</p>
-                            <p> {product.description}</p>
-
-                            <div className='PID-seller-avg-stars'>
-                                <p className='PID-seller'>Seller: {product.Seller.username}   </p>
-
+            <div className="column-holder">
+                <div className="column">
+                    <div className='PID-product'>
+                        <div className='PID-all-Images'>
+                            <div className='PID-images-div'>
+                                <img className='PID-small-img' onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/9b902f/4473276055/il_794xN.4473276055_fcxk.jpg" alt="filler image 2"></img>
+                                <img className='PID-small-img' onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/2f8cab/4425918302/il_794xN.4425918302_1x8r.jpg" alt="filler image 3"></img>
+                                <img className='PID-small-img' onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/401a30/4473276305/il_794xN.4473276305_qrxw.jpg" alt="filler image 4"></img>
+                            </div>
+                            <div className='PID-main-image-div'>
+                                <img className='PID-main-image' onClick={changeMainImage} src={mainImage} alt="filler image"></img>
                             </div>
                         </div>
-                        <div className='PID-buttons'>
-                            <button className='PID-buyNowButt'>Buy it now!</button>
-                            <button className='PID-cartButt'>Add to cart</button>
-                            <button onClick={addToFav} className='PID-favFullButt'>Add to Favorites</button>
+
+                        <div>
+                            {/* <div>
+                        <i onClick={addToFav} className="fas fa-heart" />
+                    </div> */}
+
+
+                            <div className='PID-about-buttons-div'>
+
+
+                                <div className='PID-about-product-div'>
+                                    <p>{product.price}</p>
+                                    <p> {product.description}</p>
+
+                                    <div className='PID-seller-avg-stars'>
+                                        <p className='PID-seller'>Seller: {product.Seller.username}   </p>
+
+                                    </div>
+                                </div>
+                                <div className='PID-buttons'>
+                                    <button className='PID-buyNowButt PID-P-button PID-Transp-butt'>Buy it now</button>
+                                    <button className='PID-cartButt PID-P-button'>Add to cart</button>
+                                    <button onClick={addToFav} className='PID-favFullButt PID-P-button PID-Transp-butt'> <i onClick={addToFav} className="fas fa-heart PID-heart" /> Add to Favorites</button>
+                                </div>
+
+
+                            </div>
+
                         </div>
-
-
                     </div>
+
+                    <p className='Pid-ave-stars'>
+                        <span className='PID-count'>{count} reviews</span>
+                        {stars.map((star) =>
+                            <>
+                                <i className="fas fa-star PID-count PID-stars" />
+                            </>
+                        )}
+
+                    </p>
+                    {user && <ReviewForm ></ReviewForm>}
+
+                    {productReviews.Reviews.map((review) => (
+                        <>
+                            <ReviewCard key={review.id} userFirstName={user.first_name} review={review} from="productPage" ></ReviewCard>
+                        </>
+                    ))}
+
 
                 </div>
             </div>
-
-            <p className='Pid-ave-stars'>
-                <span className='PID-count'>{count} reviews</span>
-                {stars.map((star) =>
-                    <>
-                        <i className="fas fa-star" />
-                    </>
-                )}
-
-            </p>
-            {user && <ReviewForm ></ReviewForm>}
-
-            {productReviews.Reviews.map((review) => (
-                <>
-                    <ReviewCard key={review.id} userFirstName={user.first_name} review={review} from="productPage" ></ReviewCard>
-                </>
-            ))}
-
-
-
         </>
     );
 }
