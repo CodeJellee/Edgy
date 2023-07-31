afbb2668456e9cecde5c54d79e28ed2fbcbc61fa
Minh Tran <minhttran93@gmail.com>
2023-07-30 20:57:43 -0700

    add thunk to get current user favorites

--- react-app/src/store/favorites.js
+++ react-app/src/store/favorites.js
@@ -1,10 +1,18 @@
 //* =====================  types ===========================//
 
+const GET_USER_FAVORITES_ACTION = "favorites/GET_USER_FAVORITES_ACTION";
 const POST_FAVORITE_PRODUCT_ACTION = "favorites/POST_FAVORITE_PRODUCT_ACTION";
 
 //*  ===================end of types ===================//
 
 //? =====================  actions ===========================//
+
+const getUserFavoritesAction = (favs) => {
+  return {
+    type: GET_USER_FAVORITES_ACTION,
+    favs,
+  };
+};
 
 const postFavoriteProductAction = (res) => {
   return {
@@ -16,6 +24,18 @@ const postFavoriteProductAction = (res) => {
 //?  ======================= end of actions ===================//
 
 //*  =====================  thunks ===========================//
+
+export const thunkGetUserFavorites = (userId) => async (dispatch) => {
+  let favs = await fetch(`/api/favorites/current`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  favs = await favs.json();
+  dispatch(getUserFavoritesAction(favs));
+  return favs;
+};
 
 export const thunkPostFavoriteProduct =
   (productId, userId) => async (dispatch) => {
@@ -40,19 +60,30 @@ export const thunkPostFavoriteProduct =
 
 /**
  favorites: {
-          user: {
-            userData
-          },
-          [productId]: {
-               productData
-          }
-     }
+      user: {
+        userData
+      },
+      userFavorites: {
+        [productId]: {
+            productData
+        }
+      }
+    }
  */
 
 let initialState = { user: {}, userFavorites: {} };
 export default function reducer(state = initialState, action) {
   let newState;
   switch (action.type) {
+    case GET_USER_FAVORITES_ACTION: {
+      newState = { ...state };
+      console.log("this is the favs in the reducer =======>", action.favs);
+      action.favs.Favorites.Products.forEach(
+        (product) => (newState.userFavorites[product.id] = { ...product })
+      );
+      newState.user = { ...action.favs.Favorites.User };
+      return newState;
+    }
     case POST_FAVORITE_PRODUCT_ACTION: {
       newState = { ...state };
       newState.user = action.res.User;
