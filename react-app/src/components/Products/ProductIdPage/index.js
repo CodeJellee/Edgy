00275ee58632572cb207b1f3e864a83924a638ab
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-07-31 17:53:22 -0400

    product page, more done

--- react-app/src/components/Products/ProductIdPage/index.js
+++ react-app/src/components/Products/ProductIdPage/index.js
@@ -1,9 +1,13 @@
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from "react";
 import * as reviewsActions from '../../../store/reviews'
+import * as productsActions from '../../../store/products'
 import { useSelector, useDispatch } from "react-redux";
 import ReviewForm from "../../Reviews/ReviewForm"
 import ReviewCard from "../../Reviews/ReviewCard";
+import './ProductIdPage.css'
+
+
 
 function ProductIdPage() {
     const { id } = useParams()
@@ -14,7 +18,7 @@ function ProductIdPage() {
     // conditional render should also check to make sure product is not being sold by the login user
     const user = useSelector(state => state.session.user)
     const productReviews = useSelector(state => state.reviews.productReviews)
-
+    const product = useSelector(state => state.products.singleProduct)
     // !!! default should be set to preview img
     // !   product.previewImage
     const [mainImage, setMainImage] = useState("https://i.etsystatic.com/31560168/r/il/4336cd/4425918630/il_794xN.4425918630_38m9.jpg")
@@ -34,41 +38,87 @@ function ProductIdPage() {
 
 
         dispatch(reviewsActions.thunkGetReviewsByProductId(id))
-
+        dispatch(productsActions.thunkGetSingleProduct(id))
 
 
     }, [dispatch])
 
-    if (!productReviews.Reviews) return <h1>...loading</h1>
-    console.log(productReviews, "HHHHHHHHHHHIIIIIIIIII")
-    console.log(user)
+    if (!productReviews.Reviews || !product.Reviews) return <h1>...loading</h1>
+
+
+    let sum = 0
+    let count = product.Reviews.length
+
+    let products = Object.values(product.Reviews)
+    for (const singleProduct of products) {
+
+        sum += singleProduct.stars
+    }
+
+    let starAvg = Math.round(sum / count)
+    console.log("Count:", count, "Sum:", sum, "Star average:", starAvg)
+    let stars = []
+    for (let i = 0; i < starAvg; i++) {
+        stars.push("hi")
+    }
+
+
+
+    console.log(product)
     return (
         <>
             {/* product info */}
             <div className='PID-product'>
                 <div className='PID-all-Images'>
                     <div className='PID-images-div'>
-                        <img onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/9b902f/4473276055/il_794xN.4473276055_fcxk.jpg" alt="filler image 2"></img>
-                        <img onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/2f8cab/4425918302/il_794xN.4425918302_1x8r.jpg" alt="filler image 3"></img>
-                        <img onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/401a30/4473276305/il_794xN.4473276305_qrxw.jpg" alt="filler image 4"></img>
+                        <img className='PID-small-img' onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/9b902f/4473276055/il_794xN.4473276055_fcxk.jpg" alt="filler image 2"></img>
+                        <img className='PID-small-img' onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/2f8cab/4425918302/il_794xN.4425918302_1x8r.jpg" alt="filler image 3"></img>
+                        <img className='PID-small-img' onClick={changeMainImage} src="https://i.etsystatic.com/31560168/r/il/401a30/4473276305/il_794xN.4473276305_qrxw.jpg" alt="filler image 4"></img>
                     </div>
-                    <img className='PID-main-image' onClick={changeMainImage} src={mainImage} alt="filler image"></img>
+                    <div className='PID-main-image-div'>
+                        <img className='PID-main-image' onClick={changeMainImage} src={mainImage} alt="filler image"></img>
+                    </div>
                 </div>
-                <button className='PID-favButt' onclick={addToFav}> <i className="fas fa-heart" /></button>
-                <div className='PID-about-product-div'>
-                    product.price
-                    product.description
-                    <div className='PID-seller-avg-stars'>
-                        <p className='PID-seller'>  seller.first_name </p>
-                        <p className='Pid-ave-stars'>reviews.stars.average</p></div>
-                </div>
-                <div className='PID-buttons'>
-                    <button className='PID-buyNowButt'>Buy it now!</button>
-                    <button className='PID-cartButt'>Add to cart</button>
-                    <button className='PID-favFullButt'>Add to Favorites</button>
+
+                <div>
+                    <div>
+                        <i onClick={addToFav} className="fas fa-heart" />
+                    </div>
+
+
+                    <div className='PID-about-buttons-div'>
+
+
+                        <div className='PID-about-product-div'>
+                            <p>{product.price}</p>
+                            <p> {product.description}</p>
+
+                            <div className='PID-seller-avg-stars'>
+                                <p className='PID-seller'>Seller: {product.Seller.username}   </p>
+
+                            </div>
+                        </div>
+                        <div className='PID-buttons'>
+                            <button className='PID-buyNowButt'>Buy it now!</button>
+                            <button className='PID-cartButt'>Add to cart</button>
+                            <button onClick={addToFav} className='PID-favFullButt'>Add to Favorites</button>
+                        </div>
+
+
+                    </div>
+
                 </div>
             </div>
 
+            <p className='Pid-ave-stars'>
+                <span className='PID-count'>{count} reviews</span>
+                {stars.map((star) =>
+                    <>
+                        <i className="fas fa-star" />
+                    </>
+                )}
+
+            </p>
             {user && <ReviewForm ></ReviewForm>}
 
             {productReviews.Reviews.map((review) => (
@@ -78,7 +128,7 @@ function ProductIdPage() {
             ))}
 
 
-            <h1>hi</h1>
+
         </>
     );
 }
