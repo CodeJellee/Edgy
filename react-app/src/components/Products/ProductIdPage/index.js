26b40f056bfd9d74f5296dd4ecde2f6d9cc1fb58
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-07-31 00:18:07 -0400

    review form added to productIdPage and works to post, need error handling and clear after submit

--- react-app/src/components/Products/ProductIdPage/index.js
+++ react-app/src/components/Products/ProductIdPage/index.js
@@ -2,9 +2,11 @@ import { useParams } from 'react-router-dom'
 import { useEffect } from "react";
 import * as reviewsActions from '../../../store/reviews'
 import { useSelector, useDispatch } from "react-redux";
+import ReviewForm from "../../Reviews/ReviewForm"
 
 function ProductIdPage() {
     const { id } = useParams()
+    let productId = id
 
     const dispatch = useDispatch()
     // const currentUser = useSelector(state => state.session.user)
@@ -24,8 +26,8 @@ function ProductIdPage() {
 
     return (
         <>
+            <ReviewForm ></ReviewForm>
             <h1>hi</h1>
-
         </>
     );
 }
