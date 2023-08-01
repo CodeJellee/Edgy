e6daf53c6720c0d9158c2c01fa66a89e05fd8fdb
Minh Tran <minhttran93@gmail.com>
2023-07-31 21:46:53 -0700

    add the most insane amount of list/dict comprehensions

--- app/api/favorites_routes.py
+++ app/api/favorites_routes.py
@@ -4,6 +4,7 @@ from app.models.products import favorites
 from pprint import pprint
 from app.models.db import db
 from app.models.user import User
+from app.models.products import Product
 
 # prefix='/api/favorites'
 favorites_routes = Blueprint("favorites", __name__)
@@ -16,16 +17,35 @@ def get_current_favorites():
     curr_user_id = current_user.id
     user = User.query.get(curr_user_id)
 
+    user_favorites = [
+        # return kvp for k, v in product.to_dict().items() if key is not updatedAt
+        {k: v for k, v in product.to_dict().items() if k != "updatedAt"}
+        for product in user.products
+    ]
+    sellers_ids = [product["id"] for product in user_favorites]  # [2, 3, 4] for user 5
+    sellers_user_info = User.query.filter(User.id.in_(sellers_ids)).all()
+    seller_to_product_condition = {
+        user.id: user.to_dict() for user in sellers_user_info
+    }
+
+    user_favorites_and_seller_data = [
+        {
+            k: v
+            if k not in ["sellerId", "updatedAt"]
+            else seller_to_product_condition.get(v, v)
+            for k, v in product.items()
+        }
+        for product in user_favorites
+    ]
+    return {"Favorites": user_favorites_and_seller_data}
+
     # The FINAL Form
     if user.products:
         user_favorited_products = {
             "Favorites": {
                 "User": user.to_dict(),
                 # Including all the kvp BESIDES 'updatedAt'
-                "Products": [
-                    {k: v for k, v in product.to_dict().items() if k != "updatedAt"}
-                    for product in user.products
-                ],
+                "Products": user_favorites,
             }
         }
         return user_favorited_products
