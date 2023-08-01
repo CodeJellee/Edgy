dc5fff6c053aa2ada61afdd1d3a7018876a7ee99
Minh Tran <minhttran93@gmail.com>
2023-08-01 14:51:01 -0700

    add some css to favorites component page

--- react-app/src/components/ShoppingCart/ShoppingCartPage/index.js
+++ react-app/src/components/ShoppingCart/ShoppingCartPage/index.js
@@ -1,70 +1,75 @@
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
 import { useEffect } from "react";
-import * as CartActions from '../../../store/shoppingCart' //this will be grabbing all of our thunks/reducer from the store file
+import * as CartActions from "../../../store/shoppingCart"; //this will be grabbing all of our thunks/reducer from the store file
 import { useSelector, useDispatch } from "react-redux";
-import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
-import OpenModalButton from '../../OpenModalButton';
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import OpenModalButton from "../../OpenModalButton";
 
+function ShoppingCartPage() {
+  const { id } = useParams();
 
+  const dispatch = useDispatch();
 
-function ShoppingCartPage(){
-    const { id } = useParams()
+  const sessionUser = useSelector((state) => state.session.user);
+  const userCart = useSelector((state) =>
+    Object.values(state.shoppingCart.userCart)
+  );
+  // console.log('THIS IS USERCART', Object.values(userCart))
+  //had to insert Object.values to const userCart because item was not rendering properly, bc of that no need to Object.values in the return at the bottom/html section for userCart
+  //wasn't working with Object.values and chaining it with .map
 
-    const dispatch = useDispatch();
+  const itemLength = userCart.length;
+  // console.log('length of cart here', itemLength)
 
-    const sessionUser = useSelector((state) => state.session.user);
-    const userCart = useSelector((state) => Object.values(state.shoppingCart.userCart));
-    // console.log('THIS IS USERCART', Object.values(userCart))
-    //had to insert Object.values to const userCart because item was not rendering properly, bc of that no need to Object.values in the return at the bottom/html section for userCart
-    //wasn't working with Object.values and chaining it with .map
+  useEffect(() => {
+    if (sessionUser) {
+      //if user is logged in, dispatch thunk
+      dispatch(CartActions.thunkGetShoppingCart());
+      // dispatch(CartActions.thunkDeleteCartItem(productId));
+    }
+  }, [dispatch, sessionUser]);
 
-    const itemLength = userCart.length
-    // console.log('length of cart here', itemLength)
+  if (!sessionUser) return null;
+  if (userCart.length === 0 || !userCart) return null;
 
-    useEffect(() => {
-        if (sessionUser){
-            //if user is logged in, dispatch thunk
-            dispatch(CartActions.thunkGetShoppingCart());
-            dispatch(CartActions.thunkDeleteCartItem(productId));
-        }
-    }, [dispatch, sessionUser])
+  // console.log('THIS IS ID', item)
 
-
-    if (!sessionUser) return null;
-    if (userCart.length === 0 || !userCart) return null;
-
-    // console.log('THIS IS ID', item)
-
-
-    return (
-        <>
-            <h1>{itemLength} items in your cart</h1>
+  return (
+    <>
+      <h1>{itemLength} items in your cart</h1>
+      <div>
+        {userCart?.map((item) => (
+          <div className="cart_item_container">
             <div>
-                {userCart?.map((item) => (
-                    <div className="cart_item_container">
-                    <div>
-                        <NavLink to={`/products/${item.Product.id}`} className="items-link">
-                            <img src={item.Product.preview_imageURL} alt={item.Product.item_name}/>
-                        </NavLink>
-                        <div className="name_price_container">
-                            <NavLink to={`/products/${item.Product.id}`} className="items-link">
-                                <div>{item.Product.item_name}</div>
-                            </NavLink>
-                            <div>${item.Product.price}</div>
-                        </div>
-                    </div>
-                    <OpenModalButton
-                        buttonText="Delete"
-                        modalComponent={<DeleteItemModal productId={item.Product.id} />}
-                    />
-                    </div>
-                ))}
-                <button>Proceed To Checkout</button>
-
+              <NavLink
+                to={`/products/${item.Product.id}`}
+                className="items-link"
+              >
+                <img
+                  src={item.Product.preview_imageURL}
+                  alt={item.Product.item_name}
+                />
+              </NavLink>
+              <div className="name_price_container">
+                <NavLink
+                  to={`/products/${item.Product.id}`}
+                  className="items-link"
+                >
+                  <div>{item.Product.item_name}</div>
+                </NavLink>
+                <div>${item.Product.price}</div>
+              </div>
             </div>
-
-        </>
-    );
+            <OpenModalButton
+              buttonText="Delete"
+              // modalComponent={<DeleteItemModal productId={item.Product.id} />}
+            />
+          </div>
+        ))}
+        <button>Proceed To Checkout</button>
+      </div>
+    </>
+  );
 }
 
 export default ShoppingCartPage;
