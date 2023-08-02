02b352575293944928b221df5c49e16d6d4af8de
Minh Tran <minhttran93@gmail.com>
2023-08-01 20:29:57 -0700

    comment out console logs

--- react-app/src/components/FavoritesPage/FavoritesButton.js
+++ react-app/src/components/FavoritesPage/FavoritesButton.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 function FavoriteButton({ productId, handleUnfavoriteClick }) {
   const [solid, setSolid] = useState(true);
   //   console.log("this is solid value", solid);
-  console.log("this is the product id", productId);
+  //   console.log("this is the product id", productId);
 
   const handleFavorite = (productId) => {
     if (solid === false) {
