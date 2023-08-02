0f48adfd690709be1d9361766f20951c15420a1b
thandi mpofu <thandimpofu2003@gmail.com>
2023-08-02 16:35:59 -0500

    new productId comp

--- react-app/src/components/Products/ProductIdPage/index.js
+++ react-app/src/components/Products/ProductIdPage/index.js
@@ -77,92 +77,86 @@ function ProductIdPage() {
 
   return (
     <>
-      {/* product info */}
-      <div className="column-holder">
-        <div className="column">
-          <div className="PID-product">
-            <div className="PID-all-Images">
-              <div className="PID-images-div">
-                <img
-                  className="PID-small-img"
-                  alt="product first loaded"
-                  onClick={changeMainImage}
-                  src={product.preview_imageURL}
-                ></img>
-                {images.map((image, idx) => (
-                  <img
-                    key={idx}
-                    className="PID-small-img"
-                    onClick={changeMainImage}
-                    src={image.product_imageURL}
-                    alt="filler 2"
-                  ></img>
+        {/* product info */}
+        <div className="column-holder">
+            <div className="column">
+                <div className='PID-product'>
+                    <div className='PID-all-Images'>
+                        <div className='PID-images-div'>
+                            <img className='PID-small-img' alt="product first loaded" onClick={changeMainImage} src={product.preview_imageURL} ></img>
+                            {images.map((image, idx) => (
+
+                                <img key={idx} className='PID-small-img' onClick={changeMainImage} src={image.product_imageURL} alt="filler 2"></img>
+
+
+                            ))}
+
+                        </div>
+                        <div className='PID-main-image-div'>
+
+                            <img className='PID-main-image' onClick={changeMainImage} src={mainImage || product.preview_imageURL} alt="loading"></img>
+                        </div>
+                    </div>
+
+
+                    {/* <div>
+                    <i onClick={addToFav} className="fas fa-heart" />
+                </div> */}
+
+
+                    <div className='PID-about-buttons-div'>
+
+
+                        <div className='PID-about-product-div'>
+                            <p className="PID-price">${product.price}</p>
+                            <p> {product.description}</p>
+
+                            <div className='PID-seller-avg-stars'>
+                                <p className='PID-seller'>Seller: {product.Seller.username}   </p>
+
+                            </div>
+                        </div>
+                        <div className='PID-buttons'>
+                            <button className='PID-buyNowButt PID-P-button PID-Transp-butt'> <i class="fa-brands fas fa-cc-visa"></i> Buy it now</button>
+                            {/* <button className='PID-cartButt PID-P-button'>Add to cart</button> */}
+                            <PutCartIemToCart productId={product.id} className='PID-cartButt PID-P-button'/>
+                            <button onClick={addToFav} className='PID-favFullButt PID-P-button PID-Transp-butt'> <i onClick={addToFav} className="fas fa-heart PID-heart" /> Add to Favorites</button>
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+
+                <p className='Pid-ave-stars'>
+                    <span className='PID-count'>{count}
+                        {count === 1 ? " review" : " reviews"}
+
+                    </span>
+                    {stars.map((star, idx) =>
+
+                        <i key={idx} className="fas fa-star PID-count pStars" />
+
+                    )}
+
+                </p>
+                {user && noUserReviewExist && notSeller && <ReviewForm  ></ReviewForm>}
+
+                {productReviews.Reviews?.map((review) => (
+                    <>
+                        <ReviewCard key={review.id} userFirstName={review.User.first_name} review={review} from="productPage" ></ReviewCard>
+                    </>
                 ))}
-              </div>
-              <div className="PID-main-image-div">
-                <img
-                  className="PID-main-image"
-                  onClick={changeMainImage}
-                  src={mainImage || product.preview_imageURL}
-                  alt="loading"
-                ></img>
-              </div>
+
+
             </div>
-
-            {/* <div>
-                        <i onClick={addToFav} className="fas fa-heart" />
-                    </div> */}
-
-            <div className="PID-about-buttons-div">
-              <div className="PID-about-product-div">
-                <p className="PID-price">${product.price}</p>
-                <p> {product.description}</p>
-
-                <div className="PID-seller-avg-stars">
-                  <p className="PID-seller">
-                    Seller: {product.Seller.username}{" "}
-                  </p>
-                </div>
-              </div>
-              {user?.id ? (
-                <div className="PID-buttons">
-                  <button className="PID-buyNowButt PID-P-button PID-Transp-butt">
-                    {" "}
-                    <i class="fa-brands fas fa-cc-visa"></i> Buy it now
-                  </button>
-                  {/* <button className='PID-cartButt PID-P-button'>Add to cart</button> */}
-                  <PutCartIemToCart
-                    productId={product.id}
-                    className="PID-cartButt PID-P-button"
-                  />
-                  <button
-                    onClick={addToFav}
-                    className="PID-favFullButt PID-P-button PID-Transp-butt"
-                  >
-                    {" "}
-                    <i
-                      onClick={addToFav}
-                      className="fas fa-heart PID-heart"
-                    />{" "}
-                    Add to Favorites
-                  </button>
-                </div>
-              ) : (
-                // <button className='PID-cartButt PID-P-button'>Sign up to purchase!</button>
-                <div lassName="PID-about-product-div">
-                  Please log in or sign up to favorite or purchase the items!
-                </div>
-              )}
-            </div>
-            </div>
-            </div>
-            <FooterTwo />
-
-        </>
+        </div>
+        <FooterTwo />
+    </>
 
 
-    )
-
+);
 }
 
 
