277345c16dacdc90cc224d238f06e3edd83a5d94
Jennifer Lee <leejpac@gmail.com>
2023-08-02 12:16:07 -0400

    linked add to cart button to product page, functioning and redirects to cart

--- react-app/src/components/Products/ProductIdPage/PutItemToCart.js
+++ react-app/src/components/Products/ProductIdPage/PutItemToCart.js
@@ -1,21 +1,36 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import postItemInCartAction, { thunkPostItemInCart }  from "../../../store/shoppingCart"
-
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom";
 
 function PutCartIemToCart({productId}) {
 
     const dispatch = useDispatch()
+    const history = useHistory();
+    const currentUser = useSelector(state => state.session.user)
+    const product = useSelector(state => state.products.singleProduct)
+    // console.log('WHAT IS PRODUCT', product)
 
     const onClick = (e) => {
         e.preventDefault();
-        dispatch(thunkPostItemInCart(productId))
+        if (productBelongsToUser()) {
+            alert("You cannot add your own product to the cart.");
+        } else {
+            dispatch(thunkPostItemInCart(productId))
+            history.push("/shopping_cart")
+
+        }
     }
+
+    const productBelongsToUser = () => {
+        return product.sellerId === currentUser.id;
+    };
 
 
     return(
         <>
-        <button onClick={onClick}>Add to Cart</button>
+        <button className='PID-cartButt PID-P-button' onClick={onClick}>Add to Cart</button>
         </>
     )
 }
--- react-app/src/components/Products/ProductIdPage/index.js
+++ react-app/src/components/Products/ProductIdPage/index.js
@@ -145,8 +145,8 @@ function ProductIdPage() {
                             </div>
                             <div className='PID-buttons'>
                                 <button className='PID-buyNowButt PID-P-button PID-Transp-butt'> <i class="fa-brands fas fa-cc-visa"></i> Buy it now</button>
-                                <button className='PID-cartButt PID-P-button'>Add to cart</button>
-                                <PutCartIemToCart productId={product.id}/>
+                                {/* <button className='PID-cartButt PID-P-button'>Add to cart</button> */}
+                                <PutCartIemToCart productId={product.id} className='PID-cartButt PID-P-button'/>
                                 <button onClick={addToFav} className='PID-favFullButt PID-P-button PID-Transp-butt'> <i onClick={addToFav} className="fas fa-heart PID-heart" /> Add to Favorites</button>
                             </div>
 
