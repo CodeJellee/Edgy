5f4dcfbe5fcd597aa25f6de33653f02ecb9fc251
thandi mpofu <thandimpofu2003@gmail.com>
2023-08-02 23:00:36 -0500

    another merge

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 import ReviewsCurr from "./components/Reviews/ReviewsCurr";
@@ -29,12 +27,6 @@ function App() {
         <Switch>
           <Route exact path="/">
           <HomePage />
-          </Route>
-          <Route exact path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route exact path="/signup">
-            <SignupFormPage />
           </Route>
           <Route path="/your_reviews">
             <ReviewsCurr />
@@ -66,13 +58,8 @@ function App() {
           <Route exact path="/categories/figurines">
             <Categories category="Figurines" name="Art & Figurines / Custom artwork, figurines, and totally original paintings and prints to add to your collection" />
           </Route>
-<<<<<<< HEAD
-          <Route exact path="/products/new">
-            <NewProductForm />
-=======
           <Route exact path="/categories">
             <Categories category="All" name="" />
->>>>>>> products_routes
           </Route>
           <Route path="/products/:id">
             <ProductIdPage />
