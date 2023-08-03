3b48301acafa1815083f07e1eb10e910b568758b
Minh Tran <minhttran93@gmail.com>
2023-08-02 21:27:40 -0700

    more seed data

--- react-app/src/components/Categories/index.js
+++ react-app/src/components/Categories/index.js
@@ -11,37 +11,37 @@ import SearchWaifu from "./SearchWaifu";
 import SearchManga from "./SearchManga";
 import SearchMusic from "./SearchMusic";
 import SearchFigurines from "./SearchFigurines";
+import Stars from "../Reviews/Stars/Stars.js";
 
 function Categories({ category, name }) {
   const { products } = useSelector((state) => state.products);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(productActions.thunkGetAllProducts())
-    }, [dispatch])
+    dispatch(productActions.thunkGetAllProducts());
+  }, [dispatch]);
 
-    let info = name.split(" / ")
+  let info = name.split(" / ");
 
-    name = info[0]
-    let description = info[1]
+  name = info[0];
+  let description = info[1];
 
+  let eachProduct = Object.values(products);
 
-   let eachProduct = Object.values(products)
+  if (!eachProduct) return <h1>Loading</h1>;
 
-    if (!eachProduct) return <h1>Loading</h1>
+  eachProduct = eachProduct.filter((p) => p.category == category);
 
-    eachProduct = eachProduct.filter((p) => p.category == category)
-
-    return (
-        <>
-        <div className="page">
-            <div className="pageInfo">
-            <div className="pInfo1">
+  return (
+    <>
+      <div className="page">
+        <div className="pageInfo">
+          <div className="pInfo1">
             <h1>{name}</h1>
             <p className="des">{description}</p>
             <p className="numberR">({eachProduct.length} resultes, with Ads)</p>
-            </div>
-            <div>
+          </div>
+          <div>
             {category === "Clothing" ? <SearchClothes /> : null}
             {category === "Accessories" ? <SearchJewlery /> : null}
             {category === "Home Decor" ? <SearchHome /> : null}
@@ -50,29 +50,38 @@ function Categories({ category, name }) {
             {category === "Books" ? <SearchManga /> : null}
             {category === "Music" ? <SearchMusic /> : null}
             {category === "Figurines" ? <SearchFigurines /> : null}
-            </div>
-            </div>
+          </div>
         </div>
-        <h1 className="pageT">Find something you love</h1>
-        <div className="filters">
+      </div>
+      <h1 className="pageT">Find something you love</h1>
+      <div className="filters">
         <div className="filter1">
-            <button>Estimated Arrival<span>Anytime</span><i class="fa-solid fa-caret-down"></i></button>
-            <button><i class="fa-solid fa-filter"></i>All Filters</button>
+          <button>
+            Estimated Arrival<span>Anytime</span>
+            <i class="fa-solid fa-caret-down"></i>
+          </button>
+          <button>
+            <i class="fa-solid fa-filter"></i>All Filters
+          </button>
         </div>
         <div className="filter2">
-        <button><span>Sort by:</span>Revelancy<i class="fa-solid fa-caret-down"></i></button>
+          <button>
+            <span>Sort by:</span>Revelancy<i class="fa-solid fa-caret-down"></i>
+          </button>
         </div>
-        </div>
-        <div className="products">
-        {eachProduct.map((p) =>
+      </div>
+      <div className="products">
+        {eachProduct.map((p) => (
           <div className="p">
             <img src={p.preview_imageURL}></img>
             <p className="itemName">{p.item_name}</p>
             <Stars reviews={p.Reviews}></Stars>
             <p>${p.price}</p>
-            <p>Ad by {p.Seller.first_name} {p.Seller.last_name}</p>
+            <p>
+              Ad by {p.Seller.first_name} {p.Seller.last_name}
+            </p>
           </div>
-        )}
+        ))}
       </div>
       <FooterTwo />
     </>
