c35cda773724e9ea574e9ec10cb6162f4d0d0b0b
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-08-03 14:21:38 -0400

    deleter reiew works, current user review works

--- migrations/versions/cea38cbc5535_.py
+++ migrations/versions/cea38cbc5535_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 741203f0011a
+Revision ID: cea38cbc5535
 Revises: 
-Create Date: 2023-08-03 01:51:31.445252
+Create Date: 2023-08-03 14:18:25.954982
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '741203f0011a'
+revision = 'cea38cbc5535'
 down_revision = None
 branch_labels = None
 depends_on = None
