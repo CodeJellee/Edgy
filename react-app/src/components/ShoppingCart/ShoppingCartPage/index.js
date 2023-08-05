a6be52a3c61b10355fb5737acfc377a3e9709e2a
Jennifer Lee <leejpac@gmail.com>
2023-08-05 17:39:10 -0400

    empty cart message

--- react-app/src/components/ShoppingCart/ShoppingCartPage/index.js
+++ react-app/src/components/ShoppingCart/ShoppingCartPage/index.js
@@ -34,7 +34,9 @@ function ShoppingCartPage() {
   }, [dispatch]);
 
   if (!sessionUser) return null;
-  if (userCart.length === 0 || !userCart) return null;
+  // if (userCart.length === 0 || !userCart) {
+  //   // return <h1>Your cart is empty! Time to go shopping!</h1>
+  // };
 
   // const handleRemoveItem = (productId) => {
   //     // Call the thunkDeleteCartItem action creator to remove the item from the cart.
@@ -44,14 +46,113 @@ function ShoppingCartPage() {
   // console.log("THIS IS ID", item);
 
   return (
-    <>
-      <div className="primary-cart-container">
-        <h1>{itemLength} items in your cart</h1>
-        <div className="products-and-checkout-container">
+    // <>
+    //   <div className="primary-cart-container">
+    //     <h1>{itemLength} items in your cart</h1>
+    //     <div className="products-and-checkout-container">
 
-          <div className="products-only-container">
-            {userCart?.map((item) => (
-              <>
+    //       <div className="products-only-container">
+    //         {userCart?.map((item) => (
+    //           <>
+    //             {/* {console.log(item)} */}
+    //             <div className="each-cart-item-container">
+    //               <div>
+    //                 <div className="sellerId-container">
+    //                   Purchasing from Seller {item.Product.sellerId}
+    //                 </div>
+    //               </div>
+    //               <div>
+    //                 <div className="image-title-price-container">
+    //                   <NavLink
+    //                     to={`/products/${item.Product.id}`}
+    //                     className="preview-image"
+    //                   >
+    //                     <img
+    //                       className="cart-product-image"
+    //                       src={item.Product.preview_imageURL}
+    //                       alt={item.Product.item_name}
+    //                     />
+    //                   </NavLink>
+
+    //                   <div className="name-blurb-cost-quantity">
+    //                     <div className="name-description">
+    //                       <NavLink
+    //                         to={`/products/${item.Product.id}`}
+    //                         className="items-link"
+    //                       >
+    //                         <div>{item.Product.item_name}</div>
+    //                       </NavLink>
+    //                       <div className="item-description">
+    //                         {item.Product.description}
+    //                       </div>
+    //                     </div>
+    //                     <div className="price-how-many-is-left">
+    //                       <div>${item.Product.price} Each</div>
+    //                       <div>Quantity:</div>
+    //                       <input type="number" className="quantity-input" name="quantity" min="1" defaultValue="1"/>
+    //                     </div>
+    //                   </div>
+    //                 </div>
+    //               </div>
+
+    //               <div className="remove-save">
+    //                 <DeleteCartIem
+    //                   cartItemId={item.Product.id}
+    //                 />
+    //                 <div id="remove-save-button" className='PID-favFullButt PID-P-button PID-Transp-butt'>Save for Later</div>
+    //               </div>
+
+    //               <div className="note-and-delivery">
+    //                 <div className="add-note">+ Add a note to Seller</div>
+    //                 <div className="delivery-container">
+    //                   <select name="delivery-drop-down">
+    //                     <option value="Shipping Method">Shipping Method</option>
+    //                     <option value="Free Shipping">FREE SHIPPING</option>
+    //                     <option value="Next Day">Next Day</option>
+    //                     <option value="2-3 Day">2-3 Day</option>
+    //                   </select>
+    //                   <div className="est-delivery">Estimated Delivery</div>
+    //                 </div>
+    //               </div>
+
+    //               <div className="gift-coupon">
+    //                 <div>
+    //                   <div className="gift-toggle">
+    //                     <input type="checkbox" class="toggle-input" />
+    //                     <div>This order is a gift</div>
+    //                   </div>
+    //                   <div className="gift-order-blurb">
+    //                     Prices will not be shown on the packing slip
+    //                   </div>
+    //                 </div>
+    //                 <div className="coupon">Apply shop coupon codes</div>
+    //               </div>
+    //             </div>
+    //           </>
+    //         ))}
+    //       </div>
+
+    //       <div className="checkout-button-container">
+    //         <div>
+    //           <ProceedToCheckout />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <Footer2/>
+    // </>
+
+    <>
+    <div className="primary-cart-container">
+      {userCart.length === 0 || !userCart ? (
+        <h1 className="empty-cart-message">Your cart is empty! Time to go shopping!</h1>
+      ) : (
+        <>
+          <h1>{itemLength} items in your cart</h1>
+          <div className="products-and-checkout-container">
+            <div className="products-only-container">
+              {userCart?.map((item) => (
+                <>
                 {/* {console.log(item)} */}
                 <div className="each-cart-item-container">
                   <div>
@@ -127,18 +228,20 @@ function ShoppingCartPage() {
                   </div>
                 </div>
               </>
-            ))}
-          </div>
-
-          <div className="checkout-button-container">
-            <div>
-              <ProceedToCheckout />
+              ))}
+            </div>
+            <div className="checkout-button-container">
+              <div>
+                <ProceedToCheckout />
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <Footer2/>
+        </>
+      )}
+    </div>
+    <Footer2/>
     </>
+
   );
 }
 
