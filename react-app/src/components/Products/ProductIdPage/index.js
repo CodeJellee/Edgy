10dd6bdc1cfce3f20b178b132c1dee408a9658db
Jennifer Lee <leejpac@gmail.com>
2023-08-06 00:59:00 -0400

    updated button

--- react-app/src/components/Products/ProductIdPage/index.js
+++ react-app/src/components/Products/ProductIdPage/index.js
@@ -46,6 +46,11 @@ function ProductIdPage() {
         setMainImage(e.target.src)
     }
 
+    const comingSoonFeature = (e) => {
+        e.preventDefault();
+        alert("Feature coming soon!");
+    }
+
     const addToFav = (e) => { }
 
     useEffect(() => {
@@ -170,7 +175,7 @@ function ProductIdPage() {
                             </div>
                             {user?.id ? (
                                 <div className='PID-buttons'>
-                                    <button className='PID-buyNowButt PID-P-button PID-Transp-butt'> <i class="fa-brands fas fa-cc-visa"></i> Buy it now</button>
+                                    <button className='PID-buyNowButt PID-P-button PID-Transp-butt' onClick={comingSoonFeature}> <i class="fa-brands fas fa-cc-visa"></i> Buy it now</button>
                                     {/* <button className='PID-cartButt PID-P-button'>Add to cart</button> */}
                                     <PutCartItemToCart productId={product.id} className='PID-cartButt PID-P-button' />
                                     {/* <button onClick={addToFav} className='PID-favFullButt PID-P-button PID-Transp-butt'> <i onClick={addToFav} className="fas fa-heart PID-heart" /> Add to Favorites</button> */}
